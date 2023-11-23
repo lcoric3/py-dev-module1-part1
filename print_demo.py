5471249dcaddb20566d20ec430a18091f2f6d3d8
lcoric3 <luka.coric3@gmail.com>
2023-11-23 20:14:24 +0000

    Added print demo

---
+++ print_demo.py
@@ -0,0 +1,19 @@
+# help(print)
+
+first_name = "Pero"
+last_name = "Peric"
+phone = "+385 9_1234 567"
+end_value = " "
+
+#print()
+#print(first_name, end=" ")
+#print(first_name,last_name,phone)
+
+#print(first_name,last_name,phone, sep=", ")
+
+#print(first_name,end=" ")
+#print(last_name,end=" ")
+#print(phone)
+
+print(first_name, end=" ")
+print(last_name,phone, sep=", ", end= end_value)
